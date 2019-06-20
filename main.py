47505603fb2614e200026d617a275723a9f53178
Jordan Brandes <jordanbrandes@yahoo.com>
2019-06-20 10:05:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+import string 
+import time
+
+def ran_gen(size, chars=string.ascii_letters):
+    return ''.join(random.choice(chars) for x in range(1000))
+ 
+
+max_time = int(input('Enter the amount of seconds you want to run this: '))
+start_time = time.time()  # remember when we started
+while (time.time() - start_time) < max_time:
+    print(list(ran_gen(10, "abcdef")))
